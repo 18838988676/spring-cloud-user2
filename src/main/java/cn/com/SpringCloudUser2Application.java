c60f634067bb5cdf4cc50119b3ae919697a066c3
18838988676@163.com <ww@ww-PC>
2019-03-10 22:02:14 +0800

    统一命名服务

---
+++ src/main/java/cn/com/SpringCloudUser2Application.java
@@ -0,0 +1,14 @@
+package cn.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class SpringCloudUser2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudUser2Application.class, args);
+	}
+
+}
---
+++ src/main/java/cn/com/controller/UserController.java
@@ -0,0 +1,44 @@
+package cn.com.controller;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import cn.com.pojo.UserPo;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	 private static Logger logger = Logger.getLogger(UserController.class);  
+	
+	 //服务发现客户端
+	 @Autowired
+	 private DiscoveryClient discoveryClient=null;
+	 
+	 //获取用户信息
+	 @GetMapping("/user/{id}")
+	 public UserPo getUserPo(@PathVariable("id") long id) {
+		 	ServiceInstance serviceInstance= discoveryClient.getInstances("WMCUSER").get(0);
+		 	logger.error("serviceInstance.getUri():"+serviceInstance.getUri());
+		 	logger.error("serviceInstance.getUri():"+serviceInstance.getHost());
+		 	logger.error("serviceInstance.getUri():"+serviceInstance.getUri());
+		 	UserPo userPo=new UserPo();
+		 	userPo.setId(id);
+		 	userPo.setLevel((int) (id%2+1));
+		 	userPo.setUserName("user_name_"+id);
+		 	userPo.setNote("note_"+id);
+		 	return userPo;
+	 }
+	 
+	
+	
+
+}
